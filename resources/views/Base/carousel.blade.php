8e848bb3472e0dda503a0f5d201c207b305b2063
Hung <mhungict99@gmail.com>
2021-04-16 13:07:16 +0700

    add base + rm some folder unnesscssary

---
+++ resources/views/Base/carousel.blade.php
@@ -0,0 +1,19 @@
+{{-- Owl carousel --}}
+<link rel="stylesheet" href="{{ asset('vendor/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ asset('Base/css/my-carousel.css') }}">
+{{-- Content --}}
+<div class="banner__carousel">
+    <div class="owl-carousel owl-theme">
+        @foreach ($carouselLists as $carouselItem)
+            <div class="banner__carousel-item">
+                <img src={{$carouselItem['img']}}>
+                <div id="owl-item-0" class="banner__carousel-text">
+                    <div class="banner__carousel-title">{{ $carouselItem['title']}}</div>
+                    <div class="banner__carousel-content">{{ $carouselItem['description']}}</div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/', function () {
-    // dd(DB::table('test')->get());
-    // https://media.foody.vn/images/beauty-upload-api-675x355-%282%29-210412170953.jpg
     return view('Home/home');
 })->name('home');
 
