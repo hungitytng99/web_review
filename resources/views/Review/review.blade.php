762cd2233cbcf42729c1a946ed70ec61730ab14b
vu <thanthanh160799@gmail.com>
2021-06-07 15:21:46 +0700

    update css

--- resources/views/Review/review.blade.php
+++ resources/views/Review/review.blade.php
@@ -45,9 +45,9 @@
     @endphp
     @include('Layouts.header')
 
-    <div class="booking-table">
+    <div class="row review__detail">
         <div class="container">
-            <div class="row review__detail">
+            <div class="row">
                 <div class="col-5 ">
                     <div class="review__img-box">
                         <img src="{{$restaurantDetail[0]->image}}" alt="trinh-coffee" class="review__img">
@@ -71,13 +71,18 @@
                     </ul>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="booking-table">
+        <div class="container">
+
 
             <div class="now-menu-restaurant">
                 <div class="menu-restaurant-tab">
                     <div class="item active">Thực đơn</div>
                 </div>
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-4" style="padding-right :0px">
                         <div class="menu-restaurant-category">
                             <div class="list-category" id="scroll-spy">
                                 <div class="scrollbar-container ps">
@@ -297,39 +302,40 @@
             @endphp
             @include('ReviewBanner.carousel',$carouselLists) -->
 
-            <div id="type" style="background-image: url(HomePage/images/slide_3.jpg);"
-                data-stellar-background-ratio="0.5">
-                <div class="overlay"></div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-3 to-animate">
-                            <div class="type">
-                                <h3 class="with-icon icon-1">Review</h3>
-                                <p>Tập hợp cảm nhận về những quán ăn, món ăn. Đem lại sự lựa chọn hoàn hảo cho bạn.
-                                </p>
-                            </div>
+        </div>
+    </div>
+    <div>
+        <div id="type" style="background-image: url(HomePage/images/slide_3.jpg);" data-stellar-background-ratio="0.5">
+            <div class="overlaybar"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 to-animate">
+                        <div class="type">
+                            <h3 class="with-icon icon-1">Review</h3>
+                            <p>Tập hợp cảm nhận về những quán ăn, món ăn. Đem lại sự lựa chọn hoàn hảo cho bạn.
+                            </p>
                         </div>
-                        <div class="col-md-3 to-animate">
-                            <div class="type">
-                                <h3 class="with-icon icon-2">Quán ăn</h3>
-                                <p>Quán ăn, nhà hàng ngon, nổi tiếng, thường xuyên có khuyến mãi, giúp bạn tiết kiệm
-                                    chi
-                                    phí một cách tối đa nhất.</p>
-                            </div>
+                    </div>
+                    <div class="col-md-3 to-animate">
+                        <div class="type">
+                            <h3 class="with-icon icon-2">Quán ăn</h3>
+                            <p>Quán ăn, nhà hàng ngon, nổi tiếng, thường xuyên có khuyến mãi, giúp bạn tiết kiệm
+                                chi
+                                phí một cách tối đa nhất.</p>
                         </div>
-                        <div class="col-md-3 to-animate">
-                            <div class="type">
-                                <h3 class="with-icon icon-3">Món ăn</h3>
-                                <p>Những món ăn hấp dẫn, ngon miệng, thu hút người mua trong tuần, trong tháng.</p>
-                            </div>
+                    </div>
+                    <div class="col-md-3 to-animate">
+                        <div class="type">
+                            <h3 class="with-icon icon-3">Món ăn</h3>
+                            <p>Những món ăn hấp dẫn, ngon miệng, thu hút người mua trong tuần, trong tháng.</p>
                         </div>
-                        <div class="col-md-3 to-animate">
-                            <div class="type">
-                                <h3 class="with-icon icon-4">Thực đơn</h3>
-                                <p>Đem laị cho bạn thực đơn theo chế độ dinh dưỡng, phù hợp với công việc và lối
-                                    sống
-                                    của bạn.</p>
-                            </div>
+                    </div>
+                    <div class="col-md-3 to-animate">
+                        <div class="type">
+                            <h3 class="with-icon icon-4">Thực đơn</h3>
+                            <p>Đem laị cho bạn thực đơn theo chế độ dinh dưỡng, phù hợp với công việc và lối
+                                sống
+                                của bạn.</p>
                         </div>
                     </div>
                 </div>
